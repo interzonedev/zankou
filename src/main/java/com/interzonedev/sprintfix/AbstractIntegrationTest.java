44bb047ad5c5a0cd65afb1a2faaac091646437a1
Mark Markarian <mark@interzonedev.com>
2012-02-11 22:46:24 -0800

    Corrected the location of the application context file.

--- src/main/java/com/interzonedev/sprintfix/AbstractIntegrationTest.java
+++ src/main/java/com/interzonedev/sprintfix/AbstractIntegrationTest.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(IntegrationTestRunner.class)
-@ContextConfiguration(locations = { "classpath:spring/applicationContext-integrationTest.xml" })
+@ContextConfiguration(locations = { "classpath:spring/applicationContext-sprintfix.xml" })
 public abstract class AbstractIntegrationTest {
 }
