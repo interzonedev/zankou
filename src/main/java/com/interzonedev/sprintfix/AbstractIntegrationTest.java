7dcabee3a1b61ff43bbdaa13d0b2229f586f13d9
Mark Markarian <mark@interzonedev.com>
2012-04-22 23:26:27 -0700

    Upped version to 0.2.4.
    Started adding Javadoc comments.

--- src/main/java/com/interzonedev/sprintfix/AbstractIntegrationTest.java
+++ src/main/java/com/interzonedev/sprintfix/AbstractIntegrationTest.java
@@ -3,6 +3,16 @@ package com.interzonedev.sprintfix;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 
+import com.interzonedev.sprintfix.dataset.DataSet;
+import com.interzonedev.sprintfix.dataset.DataSets;
+
+/**
+ * Top level integration test class meant to be run with a JUnit test runner. This is meant to be subclassed by any
+ * integration tests that use the {@link DataSets} or {@link DataSet} annotations on test classes and methods. Running
+ * this will create the sprintfix Spring application context.
+ * 
+ * @author "Mark Markarian" <mark@interzonedev.com>
+ */
 @RunWith(IntegrationTestRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/com/interzonedev/sprintfix/applicationContext-sprintfix.xml" })
 public abstract class AbstractIntegrationTest {
--- src/main/java/com/interzonedev/sprintfix/dataset/transformer/DataSetTransformer.java
+++ src/main/java/com/interzonedev/sprintfix/dataset/transformer/DataSetTransformer.java
@@ -1,5 +1,22 @@
 package com.interzonedev.sprintfix.dataset.transformer;
 
+import com.interzonedev.sprintfix.dataset.DataSet;
+
+/**
+ * Interface for transforming individual dataset items. When an implementation is instantiated as a bean the bean name
+ * can be set in the {@link DataSet#transformerBeanId()} annotation property.
+ * 
+ * @author Mark Markarian - mark@interzonedev.com
+ */
 public interface DataSetTransformer {
+
+	/**
+	 * Called for each item in a data set. Allows for altering the properties of the data set item being processed.
+	 * 
+	 * @param dataSetItem
+	 *            The current data set item being processed. The type and content depend on the datastore being tested.
+	 * 
+	 * @return Returns the current data set item with the properties altered as necessary.
+	 */
 	public Object transformDataSetItem(Object dataSetItem);
 }
